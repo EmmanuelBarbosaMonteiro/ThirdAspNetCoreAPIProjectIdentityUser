7e86d36b5fdb467f22010308da5ba98a49aa2b70
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-06-08 21:21:36 -0300

    Implementado a polica de acesso 'IdadeMinima' e ajustes em AcessoController

--- UsuariosApi/Program.cs
+++ UsuariosApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using UsuariosApi.Authorization;
 using UsuariosApi.Data;
 using UsuariosApi.Models;
 using UsuariosApi.Services;
@@ -28,6 +29,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("IdadeMinima", policy =>
+        policy.AddRequirements(new IdadeMinima(18))
+        );
+});
 
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<TokenService>();
